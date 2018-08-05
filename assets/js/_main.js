5d0ed27869d513bc71ca80f307008cb0e2644466
Luis Puerto <luiss.puerto@gmail.com>
2018-08-05 15:09:54 +0300

    removing some categories for anchors

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -72,6 +72,7 @@ $(document).ready(function() {
     class: 'anchorjs'
   };
   anchors.add();
+  anchors.remove('.archive__item-title, .author__name');
 
   // init smooth scroll
   $("a").smoothScroll({ offset: -20 });
