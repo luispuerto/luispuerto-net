3c36142a15fb73cc1fd1ddb2b0436e75d6b483d9
Luis Puerto <luiss.puerto@gmail.com>
2018-08-06 20:31:37 +0300

    script to add the hash (#something) to url
    it’s commented because it isn’t working with footnote backlinks.

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -77,6 +77,33 @@ $(document).ready(function() {
   // init smooth scroll
   $("a").smoothScroll({ offset: -80});
 
+  // Bind the hashchange event listener
+  // $(window).bind('hashchange', function(event) {
+  //   $.smoothScroll({
+  //     // Replace '#/' with '#' to go to the correct target
+  //     scrollTarget: location.hash.replace(/^\#\/?/, '#'),
+  //     offset: -80
+  //   });
+  // });
+  //
+  // $('a[href*="#"]')
+  // .bind('click', function(event) {
+  //   // Remove '#' from the hash.
+  //   var hash = this.hash.replace(/^#/, '')
+  //
+  //   if (this.pathname === location.pathname && hash) {
+  //     event.preventDefault();
+  //
+  //     // Change '#' (removed above) to '#/' so it doesn't jump without the smooth scrolling
+  //     location.hash = '#/' + hash;
+  //   }
+  // });
+  //
+  // // Trigger hashchange event on page load if there is a hash in the URL.
+  // if (location.hash) {
+  //   $(window).trigger('hashchange');
+  // }
+
   // add lightbox class to all image links
   $(
     "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']"
